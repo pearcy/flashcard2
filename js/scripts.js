262631135ef73593211b8823fd46358cfe1170c9
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-02 15:43:36 -0700

    add bootstrap cards with show, hide jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
   $(".card-header#one").click(function() {
-    $(".card-body").toggle();
+    $(".card-body#one").toggle();
   });
 
+  $(".card-header#two").click(function() {
+    $(".card-body#two").toggle();
+  });
+
+  $(".card-header#three").click(function() {
+    $(".card-body#three").toggle();
+  });
   
 }); // end document ready
